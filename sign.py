58b084c74e29e9de74338ed6cfa49265264c57bd
richak13 <rkhed@seas.upenn.edu>
2024-11-01 19:56:32 -0400

    Update sign.py

--- sign.py
+++ sign.py
@@ -1,1 +1,23 @@
+import eth_account
+from web3 import Web3
+from eth_account import Account
+from eth_account.messages import encode_defunct
 
+
+def sign(m):
+    w3 = Web3()
+    
+    # Generate a new Ethereum account
+    account = Account.create()
+    eth_address = account.address  # Account address derived from public key
+    private_key = account.privateKey  # Account private key for signing
+    
+    # Encode the message
+    message = encode_defunct(text=m)
+    
+    # Sign the message using the private key
+    signed_message = Account.sign_message(message, private_key=private_key)
+    
+    assert isinstance(signed_message, eth_account.datastructures.SignedMessage)
+    
+    return eth_address, signed_message
