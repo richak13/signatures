52a032bc2c4f564d23e47106fdc86992f1e52757
richak13 <rkhed@seas.upenn.edu>
2024-11-01 20:25:40 -0400

    Update sign.py

--- sign.py
+++ sign.py
@@ -2,15 +2,11 @@ from eth_account import Account
 from eth_account.messages import encode_defunct
 
 def sign(m):
-    # Generate a new Ethereum account
     account = Account.create()
-    eth_address = account.address  # Account address derived from public key
+    eth_address = account.address
     
-    # Encode the message
     message = encode_defunct(text=m)
     
-    # Sign the message using the account object
     signed_message = account.sign_message(message)
     
-    # Return both the Ethereum address and the signature
     return eth_address, signed_message
